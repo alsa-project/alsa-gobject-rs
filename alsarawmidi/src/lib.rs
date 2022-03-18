7b3193835410dbc243e6eb8dc5e6a61873f170ac
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-03-18 14:58:47 +0900

    alsarawmidi: regenerate API crate
    This commit regenerate API crate.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- alsarawmidi/src/lib.rs
+++ alsarawmidi/src/lib.rs
@@ -1,16 +1,8 @@
 // SPDX-License-Identifier: MIT
-#[macro_use]
-extern crate glib;
-#[macro_use]
-extern crate bitflags;
-extern crate alsarawmidi_sys;
-extern crate glib_sys;
-extern crate gobject_sys;
-extern crate libc;
-
 mod auto;
 mod stream_pair;
 
 pub use crate::{auto::*, stream_pair::*};
+pub use ffi;
 
-use glib::{object::IsA, translate::*};
+use glib::{translate::*, IsA};
