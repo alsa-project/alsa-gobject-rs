4354568fe6585d0a4f7fd4edb304683aef734533
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-03-18 14:58:46 +0900

    alsactl: regenerate API crate
    This commit regenerate API crate.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- alsactl/src/lib.rs
+++ alsactl/src/lib.rs
@@ -1,18 +1,10 @@
 // SPDX-License-Identifier: MIT
-#[macro_use]
-extern crate glib;
-#[macro_use]
-extern crate bitflags;
-extern crate alsactl_sys;
-extern crate glib_sys;
-extern crate gobject_sys;
-extern crate libc;
-
 mod auto;
 mod card;
 mod elem_info;
 mod elem_value;
 
 pub use crate::{auto::*, card::*, elem_info::*, elem_value::*};
+pub use ffi;
 
-use glib::{object::IsA, translate::*};
+use glib::{translate::*, IsA};
