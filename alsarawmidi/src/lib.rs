77b3cd8d83d7503f1f2bcd1a0a4b572290b25d27
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-07-07 22:07:27 +0900

    alsarawmidi: add crate README.md
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- alsarawmidi/src/lib.rs
+++ alsarawmidi/src/lib.rs
@@ -1,20 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-//! Rust bindings for alsarawmidi library
-//!
-//! Rust bindings and wrappers for alsarawmidi library in
-//! [alsa-gobject](https://github.com/alsa-project/alsa-gobject) to operate ALSA Rawmidi character
-//! device.
-//!
-//! The alsarawmidi library in alsa-gobject v0.3.0 is the minimum supported version for underlying
-//! library.
-//!
-//! The crate depends on [glib crate v0.15](https://crates.io/crates/glib) provided by
-//! [gtk-rs project](https://gtk-rs.org/) for type/object system, event loop, and dispacher.
-//!
-//! # License
-//!
-//! Released under MIT license.
+#![doc = include_str!("../README.md")]
 
 mod auto;
 mod stream_pair;
