5842f926f035df6465f014e2d3d72731f41a107a
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-07-07 22:07:27 +0900

    alsahwdep: add document alias to manual implementations
    The gir tool generate document alias to implementations generated
    automatically, while not to manual implementations.
    
    This commit adds them.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- alsahwdep/src/device_common.rs
+++ alsahwdep/src/device_common.rs
@@ -2,6 +2,7 @@
 use super::*;
 
 pub trait DeviceCommonExtManual {
+    #[doc(alias = "alsahwdep_device_common_get_protocol_version")]
     fn protocol_version(&self) -> Result<[u16; 3], glib::Error>;
 }
 
--- alsahwdep/src/enums.rs
+++ alsahwdep/src/enums.rs
@@ -3,6 +3,7 @@
 use super::*;
 
 impl DeviceCommonError {
+    #[doc(alias = "alsahwdep_device_common_error_to_label")]
     pub fn to_label(&self) -> &'static str {
         unsafe {
             let mut ptr = std::ptr::null_mut() as *const i8;
