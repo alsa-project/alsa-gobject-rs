71750c355ff8d36848e00c9c4a53953f6226793f
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2026-01-12 09:30:32 +0900

    alsaseq: declare alsaseq-sys alias inner code
    The current way to declare alsaseq-sys alias in Cargo.toml is too strong.
    
    This commit declares it in top-level code file.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- alsaseq/src/lib.rs
+++ alsaseq/src/lib.rs
@@ -32,8 +32,8 @@ pub mod prelude {
 /// For subclass implementations derived from provided class.
 pub mod subclass;
 
-// To access to alsaseq-sys crate for FFI.
-pub use ffi;
+// For applications to handle stuffs in the sys crate.
+pub use alsaseq_sys as ffi;
 
 // For links in documentation.
 pub(crate) use glib;
